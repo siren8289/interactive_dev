ef65279c6832a618f718a052a89e6a8b2365e5d8
siren8289 <siren8289@gmail.com>
2025-10-08 04:07:02 +0900

    3번째 미니 플젝

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import UniteTheVibe from "./components/UniteTheVibe";
+import MusicSection from "./components/MusicSection";
 
 export default function App() {
   return (
-    <div className="bg-black text-white min-h-screen overflow-hidden">
-      <UniteTheVibe />
+    <div className="bg-[#0A0A0A] text-white w-screen overflow-x-hidden overflow-y-auto snap-y snap-mandatory">
+      {/* 🔵 두 번째 섹션 */}
+      <section className="h-screen snap-start">
+        <MusicSection />
+      </section>
     </div>
   );
 }
